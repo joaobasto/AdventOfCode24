3e525e0b6340509514c690f56adc5f075c22be44
João Basto <23194887+joaobasto@users.noreply.github.com>
2024-12-07 21:42:54 +0000

    Initial draft of solution for Day 7 Part 1

---
+++ Solutions/src/main/java/org/example/Day7/Day7Solver.java
@@ -0,0 +1,56 @@
+package org.example.Day7;
+
+import org.example.AbstractSolver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Day7Solver extends AbstractSolver {
+
+    @Override
+    protected long createSolutionExercise1(BufferedReader br) throws IOException {
+        String line;
+        long result = 0;
+        while ((line = br.readLine()) != null) {
+            String[] numbersAsStrings = line.split(": ");
+            Long testValue = Long.valueOf(numbersAsStrings[0]);
+            List<Long> numbers = Arrays.stream(numbersAsStrings[1].split("\\s+"))
+                    .map(Long::valueOf).collect(Collectors.toList());
+
+            //define initial state
+            long currentValue = 0;
+            int currentNumberIndex = 0;
+
+            if (isValidEquation(currentValue, currentNumberIndex, testValue, numbers)) {
+                result += testValue;
+            }
+        }
+
+        return result;
+    }
+
+    private boolean isValidEquation(long currentValue, int currentNumberIndex, long testValue, List<Long> numbers) {
+        //check for terminal conditions
+        if (currentValue > testValue) {
+            return false;
+        }
+        if (currentNumberIndex == numbers.size()) {
+            return currentValue == testValue;
+        }
+        return isValidEquation(currentValue * numbers.get(currentNumberIndex), currentNumberIndex+1, testValue, numbers)
+                || isValidEquation(currentValue + numbers.get(currentNumberIndex), currentNumberIndex+1, testValue, numbers);
+    }
+
+    @Override
+    protected long createSolutionExercise2(BufferedReader br) throws IOException {
+        String line;
+        while ((line = br.readLine()) != null) {
+        }
+
+        long result = 0;
+
+        return result;
+    }
+}
--- Solutions/src/main/java/org/example/Main.java
+++ Solutions/src/main/java/org/example/Main.java
@@ -1,13 +1,14 @@
 package org.example;
 
-import org.example.Day6.Day6Solver;
+
+import org.example.Day7.Day7Solver;
 
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         long startTime = System.currentTimeMillis();
-        Solver solver = new Day6Solver();
+        Solver solver = new Day7Solver();
         solver.solveExercise1();
         solver.solveExercise2();
         long duration = System.currentTimeMillis() - startTime;
