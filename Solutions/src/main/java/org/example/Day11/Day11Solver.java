03d54f4f7394df4563a32ad53dff8df19b6e1567
João Basto <23194887+joaobasto@users.noreply.github.com>
2024-12-12 10:36:34 +0000

    Add cache

--- Solutions/src/main/java/org/example/Day11/Day11Solver.java
+++ Solutions/src/main/java/org/example/Day11/Day11Solver.java
@@ -21,6 +21,7 @@ public class Day11Solver extends AbstractSolver {
         for (long i = 0; i < N_BLINKS; i++) {
             processBlink(numbers);
             System.out.println("Processed blink number " + (i + 1));
+            System.out.println("Number of rocks " + numbers.size());
         }
 
         long result = numbers.size();
@@ -59,15 +60,15 @@ public class Day11Solver extends AbstractSolver {
                 .collect(Collectors.toList());
 
         while (!isFinished(elements)) {
+            long minBlink = elements.stream().map(Element::getBlinkStage).min(Long::compare).get();
+            System.out.println("Min blink: " + minBlink);
             for (int i = 0; i < elements.size(); i++) {
                 if (elements.get(i).getBlinkStage() == N_BLINKS) {
                     continue;
                 }
                 long value = elements.get(i).getValue();
 
-                //TODO try to advance using cache
                 if (graph.get(value) != null && graph.get(value).getLeftNode() != null) {
-                    System.out.println("Cache hit");
                     List<Element> newElements = getNextElements(graph.get(value), elements.get(i).getBlinkStage());
                     elements.addAll(newElements);
                     elements.remove(i);
@@ -78,8 +79,8 @@ public class Day11Solver extends AbstractSolver {
                 if (value == 0L) {
                     elements.get(i).setValue(1L);
                     elements.get(i).setBlinkStage(elements.get(i).getBlinkStage() + 1);
-                    Node currentNode = Optional.ofNullable(graph.get(0L)).orElse(new Node(0));
-                    Node leftNode = Optional.ofNullable(graph.get(1L)).orElse(new Node(1));
+                    Node currentNode = graph.computeIfAbsent(0L, unused -> new Node(0L));
+                    Node leftNode = graph.computeIfAbsent(1L, unused -> new Node(1L));
                     currentNode.setLeftNode(leftNode);
                     currentNode.setRightNode(null);
                 } else {
@@ -92,16 +93,16 @@ public class Day11Solver extends AbstractSolver {
                         elements.get(i).setBlinkStage(elements.get(i).getBlinkStage() + 1);
                         elements.add(i, new Element(firstNumber, elements.get(i).getBlinkStage()));
                         i++;
-                        Node currentNode = Optional.ofNullable(graph.get(value)).orElse(new Node(value));
-                        Node leftNode = Optional.ofNullable(graph.get(firstNumber)).orElse(new Node(firstNumber));
-                        Node rightNode = Optional.ofNullable(graph.get(secondNumber)).orElse(new Node(secondNumber));
+                        Node currentNode = graph.computeIfAbsent(value, unused -> new Node(value));
+                        Node leftNode = graph.computeIfAbsent(firstNumber, unused -> new Node(firstNumber));
+                        Node rightNode = graph.computeIfAbsent(secondNumber, unused -> new Node(secondNumber));
                         currentNode.setLeftNode(leftNode);
                         currentNode.setRightNode(rightNode);
                     } else {
                         elements.get(i).setValue(value * 2024);
                         elements.get(i).setBlinkStage(elements.get(i).getBlinkStage() + 1);
-                        Node currentNode = Optional.ofNullable(graph.get(value)).orElse(new Node(value));
-                        Node leftNode = Optional.ofNullable(graph.get(value*2024)).orElse(new Node(value*2024));
+                        Node currentNode = graph.computeIfAbsent(value, unused -> new Node(value));
+                        Node leftNode = graph.computeIfAbsent(value*2024, unused -> new Node(value*2024));
                         currentNode.setLeftNode(leftNode);
                         currentNode.setRightNode(null);
                     }
@@ -133,11 +134,18 @@ public class Day11Solver extends AbstractSolver {
             }
         }
         final long blinkStage2 = blinkStage;
-        return nodesToExplore.stream().map(node2 -> new Element(node2.getValue(), blinkStage2)).collect(Collectors.toList());
+        List<Element> elements = new ArrayList<>();
+        nodesToExplore.stream().map(node2 -> new Element(node2.getValue(), blinkStage2)).forEach(element -> elements.add(element));
+        return elements;
     }
 
     private boolean areValid(List<Node> nodesToExplore) {
-        return nodesToExplore.stream().allMatch(node -> node.getLeftNode() != null);
+        for (Node node : nodesToExplore) {
+            if (node.getLeftNode() == null) {
+                return false;
+            }
+        }
+        return true;
     }
 
     private boolean isFinished(List<Element> elements) {
