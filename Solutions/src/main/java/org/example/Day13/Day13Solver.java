e39927a0bdd35dd6011e0ad8a6f9462f5c978bf3
João Basto <23194887+joaobasto@users.noreply.github.com>
2024-12-14 01:24:04 +0000

    Add solution for Day 13 Part 2

--- Solutions/src/main/java/org/example/Day13/Day13Solver.java
+++ Solutions/src/main/java/org/example/Day13/Day13Solver.java
@@ -32,21 +32,21 @@ public class Day13Solver extends AbstractSolver {
             long xT = Long.parseLong(matcher.group(1));
             long yT = Long.parseLong(matcher.group(2));
 
-            if ((double)xA - ((double)yA/(double)yB)*xB == 0D) {
+            if (yA * xB == yB * xA) {
                 if (xT == yT) {
                     for (int i = 0; i <= 100; i++) {
                         long nA = i;
                         double nB = ((double)yT - nA*yA)/yB;
-                        if ((nB >= 0) && ((nB % 1) < 0.0000001 || (nB % 1) > 0.9999999)) {
+                        if ((yT - nA*yA) % yB == 0 && nB >= 0) {
                             result += nA*3 + nB;
                             break;
                         }
                     }
                 }
             } else {
-                double nA = (xT - (double)yT *  xB / yB) /((double)xA - ((double)yA/(double)yB)*xB);
+                double nA = (double) (xT * yB - yT * xB) / (xA * yB - yA * xB);
                 double nB = ((double)yT - nA*yA)/yB;
-                if ((nA >= 0) && (nB >= 0) && ((nA % 1) < 0.0000001 || (nA % 1) > 0.9999999) && ((nB % 1) < 0.0000001 || (nB % 1) > 0.9999999)) {
+                if (nA >= 0 && nB >= 0 && (xT * yB - yT * xB) % (xA * yB - yA * xB) == 0 && (yT - nA*yA) % yB == 0) {
                     result += nA*3 + nB;
                 }
             }
@@ -57,6 +57,47 @@ public class Day13Solver extends AbstractSolver {
 
     @Override
     protected long createSolutionExercise2(BufferedReader br) throws IOException {
-        return 0;
+        String line;
+        long result = 0;
+        while ((line = br.readLine()) != null) {
+            Pattern pattern = Pattern.compile("Button A: X\\+(\\d+), Y\\+(\\d+)");
+            Matcher matcher = pattern.matcher(line);
+            matcher.find();
+            long xA = Long.parseLong(matcher.group(1));
+            long yA = Long.parseLong(matcher.group(2));
+            line = br.readLine();
+            pattern = Pattern.compile("Button B: X\\+(\\d+), Y\\+(\\d+)");
+            matcher = pattern.matcher(line);
+            matcher.find();
+            long xB = Long.parseLong(matcher.group(1));
+            long yB = Long.parseLong(matcher.group(2));
+            line = br.readLine();
+            pattern = Pattern.compile("Prize: X=(\\d+), Y=(\\d+)");
+            matcher = pattern.matcher(line);
+            matcher.find();
+            long xT = Long.parseLong(matcher.group(1)) + 10000000000000L;
+            long yT = Long.parseLong(matcher.group(2)) + 10000000000000L;
+
+            if (yA * xB == yB * xA) {
+                if (xT == yT) {
+                    for (int i = 0; i <= 100; i++) {
+                        long nA = i;
+                        double nB = ((double)yT - nA*yA)/yB;
+                        if ((yT - nA*yA) % yB == 0 && nB >= 0) {
+                            result += nA*3 + nB;
+                            break;
+                        }
+                    }
+                }
+            } else {
+                double nA = (double) (xT * yB - yT * xB) / (xA * yB - yA * xB);
+                double nB = ((double)yT - nA*yA)/yB;
+                if (nA >= 0 && nB >= 0 && (xT * yB - yT * xB) % (xA * yB - yA * xB) == 0 && (yT - nA*yA) % yB == 0) {
+                    result += nA*3 + nB;
+                }
+            }
+            br.readLine();
+        }
+        return result;
     }
 }
