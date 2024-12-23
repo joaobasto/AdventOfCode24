0bf2366fbbcb7dca81591e5aa08b903e9e4d892d
João Basto <23194887+joaobasto@users.noreply.github.com>
2024-12-23 14:43:44 +0000

    Add solution for Day 17 Part 2

--- Solutions/src/main/java/org/example/Day17/Day17Solver.java
+++ Solutions/src/main/java/org/example/Day17/Day17Solver.java
@@ -4,9 +4,12 @@ import org.example.AbstractSolver;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
+
 
 public class Day17Solver extends AbstractSolver {
     @Override
@@ -18,8 +21,16 @@ public class Day17Solver extends AbstractSolver {
         String[] valuesString = br.readLine().split(": ")[1].split(",");
         List<Long> values = Arrays.stream(valuesString)
                 .map(Long::valueOf).collect(Collectors.toList());
-        long instructionCounter = 0;
 
+        List<Long> result = createOutput(registerA, registerB, registerC, values);
+        System.out.println(result);
+        return 0;
+    }
+
+    private List<Long> createOutput(long registerA, long registerB, long registerC, List<Long> values) {
+        List<Long> result = new ArrayList<>();
+
+        long instructionCounter = 0;
         while (true) {
             if (instructionCounter >= values.size()) {
                 break;
@@ -50,7 +61,7 @@ public class Day17Solver extends AbstractSolver {
             } else if (opcode == 4) {
                 registerB = registerB ^ registerC;
             } else if (opcode == 5) {
-                System.out.print(comboOperand % 8 + ",");
+                result.add(comboOperand % 8);
             } else if (opcode == 6) {
                 registerB = (long) (registerA / Math.pow(2, comboOperand));
             } else if (opcode == 7) {
@@ -58,12 +69,77 @@ public class Day17Solver extends AbstractSolver {
             }
             instructionCounter += 2;
         }
-        System.out.println();
-        return 0;
+        return result;
     }
 
     @Override
     protected long createSolutionExercise2(BufferedReader br) throws IOException {
-        return 0;
+        long registerA = Long.parseLong(br.readLine().split(": ")[1]);
+        long registerB = Long.parseLong(br.readLine().split(": ")[1]);
+        long registerC = Long.parseLong(br.readLine().split(": ")[1]);
+        br.readLine();
+        String[] valuesString = br.readLine().split(": ")[1].split(",");
+        List<Long> values = Arrays.stream(valuesString)
+                .map(Long::valueOf).collect(Collectors.toList());
+        Collections.reverse(values);
+
+
+        List<PartialSolution> partialSolutions = new ArrayList<>();
+        partialSolutions.add(new PartialSolution(0, 0));
+
+        long result = 0;
+        while (!partialSolutions.isEmpty()) {
+            PartialSolution currentPartialSolution = partialSolutions.remove(0);
+            int valueIterator = currentPartialSolution.getValueIterator();
+            result = currentPartialSolution.getResult();
+            if (valueIterator == values.size()) {
+                break;
+            }
+            for (long i = 0; i < 8; i++) {
+                long value1 = i ^ 5;
+                long value2 = result << 3;
+                value2 = value2 + i;
+                value2 = value2 >> value1;
+                value1 = value1 ^ 6;
+                value2 = value2 % 8;
+                value1 = value1 ^ value2;
+                if (value1 == values.get(valueIterator)) {
+                    partialSolutions.add(new PartialSolution(result * 8 + i, valueIterator + 1));
+                }
+            }
+        }
+        return result;
+    }
+
+    protected long createSolutionExercise2Temp(BufferedReader br) throws IOException {
+        long registerA = Long.parseLong(br.readLine().split(": ")[1]);
+        long registerB = Long.parseLong(br.readLine().split(": ")[1]);
+        long registerC = Long.parseLong(br.readLine().split(": ")[1]);
+        br.readLine();
+        String[] valuesString = br.readLine().split(": ")[1].split(",");
+        List<Long> values = Arrays.stream(valuesString)
+                .map(Long::valueOf).collect(Collectors.toList());
+        Collections.reverse(values);
+
+        long result = 0;
+        int valueIterator = 0;
+
+        while(valueIterator < values.size()) {
+            for (long i = 0; i < 8; i++) {
+                long value1 = i ^ 5;
+                long value2 = result << 3;
+                value2 = value2 + i;
+                value2 = value2 >> value1;
+                value1 = value1 ^ 6;
+                value2 = value2 % 8;
+                value1 = value1 ^ value2;
+                if (value1 == values.get(valueIterator)) {
+                    result = result * 8 + i;
+                    valueIterator++;
+                    break;
+                }
+            }
+        }
+        return result;
     }
 }
---
+++ Solutions/src/main/java/org/example/Day17/PartialSolution.java
@@ -0,0 +1,21 @@
+package org.example.Day17;
+
+public class PartialSolution {
+
+    long result;
+
+    int valueIterator;
+
+    public PartialSolution(long result, int valueIterator) {
+        this.result = result;
+        this.valueIterator = valueIterator;
+    }
+
+    public long getResult() {
+        return result;
+    }
+
+    public int getValueIterator() {
+        return valueIterator;
+    }
+}
