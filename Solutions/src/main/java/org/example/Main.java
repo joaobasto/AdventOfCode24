5ee81356d60d019b5cb77766c361c0a5b8907795
João Basto <23194887+joaobasto@users.noreply.github.com>
2025-12-09 21:57:19 +0000

    Add solution for 2025 Day 9 Part 1

--- Solutions/src/main/java/org/example/Main.java
+++ Solutions/src/main/java/org/example/Main.java
@@ -1,13 +1,13 @@
 package org.example;
 
-import org.example.Day2508.Day2508Solver;
+import org.example.Day2509.Day2509Solver;
 
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         long startTime = System.currentTimeMillis();
-        Solver solver = new Day2508Solver();
+        Solver solver = new Day2509Solver();
         solver.solveExercise1();
         solver.solveExercise2();
         long duration = System.currentTimeMillis() - startTime;
